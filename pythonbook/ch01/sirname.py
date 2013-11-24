e30c6abe5caffe1bb954114207ea3c7e7d95bd9a
Yong Choi <sk8er.choi@gmail.com>
2013-11-25 01:12:50 +0900

    corrent misspelling

--- pythonbook/ch01/sirname.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-sirname = ['김', '이', '박', '최', '강', '윤', '장', '임', '오']
-import random
-for x in range(20):
-    print(random.choice(sirname) + '서방')
---
+++ pythonbook/ch01/surname.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+surname = ['김', '이', '박', '최', '강', '윤', '장', '임', '오']
+import random
+for x in range(20):
+    print(random.choice(surname) + '서방')
