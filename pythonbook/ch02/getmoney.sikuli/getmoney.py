7caacf5f2af9ecafd2fe81f85576838b49384a8b
Yong Choi <sk8er.choi@gmail.com>
2014-01-04 14:45:44 +0900

    rename source folder name
    Sikuli crashes when an imange in the source foler has the same file name
    with parent folder. This change is work-around for avoid the problem.

---
+++ pythonbook/ch02/getmoney.sikuli/getmoney.py
@@ -0,0 +1,3 @@
+moneybag = Pattern("moneybag.png").targetOffset(0,50)
+while exists(moneybag):
+    click(moneybag)
--- pythonbook/ch02/moneybag.sikuli/moneybag.py
+++
@@ -1,3 +0,0 @@
-moneybag = Pattern("1385363714143.png").targetOffset(0,50)
-while exists(moneybag):
-    click(moneybag)
