dc89fcf8a51d6ea8b5a4cae353f93356fce7011f
Yong Choi <sk8er.choi@gmail.com>
2014-01-04 16:37:23 +0900

    add spanish examples

---
+++ pythonbook/ch04/spanish/spanish_numbers_1to100.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+number_dict = {
+    0: 'cero',
+    1: 'uno',
+    2: 'dos',
+    3: 'tres',
+    4: 'cuatro',
+    5: 'cinco',
+    6: 'seis',
+    7: 'siete',
+    8: 'ocho',
+    9: 'nueve',
+    10: 'diez',
+    11: 'once',
+    12: 'doce',
+    13: 'trece',
+    14: 'catorce',
+    15: 'quince',
+    16: 'dieciséis',
+    17: 'diecisiete',
+    18: 'dieciocho',
+    19: 'diecinueve',
+    20: 'veinte',
+    21: 'veintiuno',
+    22: 'veintidós',
+    23: 'veintitrés',
+    24: 'veinticuatro',
+    25: 'veinticinco',
+    26: 'veintiséis',
+    27: 'veintisiete',
+    28: 'veintiocho',
+    29: 'veintinueve',
+    30: 'treinta',
+    40: 'cuarenta',
+    50: 'cincuenta',
+    60: 'sesenta',
+    70: 'setenta',
+    80: 'ochenta',
+    90: 'noventa',
+    100: 'cien'
+    }        
+		
+number = int(input('숫자를 입력하세요: '))
+if number > 100:
+    print('1부터 100까지만요.')
+else:
+    if number in number_dict:
+        spanish = number_dict[number]
+    else:
+        n1 = number % 10
+        n2 = (number // 10) * 10
+        spanish = '%s y %s' % (number_dict[n2], number_dict[n1])
+    print(spanish)
---
+++ pythonbook/ch04/spanish/spanish_numbers_1to5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+number = int(input('숫자를 입력하세요: '))
+
+if number == 1:
+        spanish = 'uno'
+elif number == 2:
+        spanish = 'dos'
+elif number == 3:
+        spanish = 'tres'
+elif number == 4:
+        spanish = 'cuatro'
+elif number == 5:
+        spanish = 'cinco'
+else:
+        spanish = ''
+
+print(spanish)
